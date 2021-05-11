219a045e88a0ae2ed15e97a77bfde89c92eb9701
Balaji Venkatachalam <balaji.v.17091982@gmail.com>
2021-05-11 16:23:10 +0530

    changed the app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> BALAJI's WEB PAGE
+          Edit <code>src/App.js</code> Changed in the feature branch
         </p>
         <a
           className="App-link"
